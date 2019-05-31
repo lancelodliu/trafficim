84db6554bba3c6d5350b33f8ac408062bc16dd4c
Yujie Liu <yujil@microsoft.com>
2019-05-31 14:26:45 +0800

    webpack ok

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Phaser from "phaser";
+import bg from "./assets/bg.png"
+import car from "./assets/car.png"
 
 const config = {
     type: Phaser.AUTO,
@@ -20,30 +22,12 @@ const game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('bg', bg);
+    this.load.image('car', car);
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    this.add.image(0, 0, 'bg');
+    this.add.image(100, 0, 'car');
 }
---
+++ webpack/base.js
@@ -0,0 +1,61 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        include: [
+          path.resolve(__dirname, "./src")
+        ],
+        exclude: /(node_modules|dist)/,
+        use: ["babel-loader", "ts-loader"]
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader"
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: "file-loader"
+      }
+    ]
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  devServer: {
+    proxy: { // proxy URLs to backend development server
+      '/': 'http://localhost:3000'
+    },
+    contentBase: path.join(__dirname, 'dist'), // boolean | string | array, static file location
+    compress: true, // enable gzip compression
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    https: false, // true for self-signed, object for cert authority
+    noInfo: true, // only errors & warns on hot reload
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
